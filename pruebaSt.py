fdb758239d697e2df77b7ac83783d7078f3bdbbc
Leticia-Lemus <leticcia.lemux@gmail.com>
2025-05-31 07:26:06 -0600

    Update pruebaSt.py

--- pruebaSt.py
+++ pruebaSt.py
@@ -26,12 +26,12 @@ def dataframe_a_contexto(df):
     return df.to_csv(index=False)
 
 client = OpenAI(api_key=openai_api_key)
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    st.chat_message("user").write(prompt)
-    response = client.chat.completions.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
-    msg = response.choices[0].message.content
-    st.session_state.messages.append({"role": "assistant", "content": msg})
-    st.chat_message("assistant").write(msg)
+st.session_state.messages.append({"role": "user", "content": prompt})
+st.chat_message("user").write(prompt)
+response = client.chat.completions.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
+msg = response.choices[0].message.content
+st.session_state.messages.append({"role": "assistant", "content": msg})
+st.chat_message("assistant").write(msg)
 
 
 # Función para generar respuesta con OpenAI
